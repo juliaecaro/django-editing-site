5f5ad273514a9df1fca6cfe5ba3011ad767c905b
Julia Caro <juliaecaro@gmail.com>
2024-08-20 18:01:09 -0500

    Update settings.py

--- truedit/settings.py
+++ truedit/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = ['truedits.onrender.com', '127.0.0.1']
 
