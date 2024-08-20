42466285fa1ca547cb07d83cc34d758ad8618b0a
Julia Caro <juliaecaro@gmail.com>
2024-08-20 16:02:33 -0500

    Update settings.py

--- truedit/settings.py
+++ truedit/settings.py
@@ -217,7 +217,7 @@ if 'DATABASE_URL' in os.environ:
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
